bb7795615cec528d08c1aba56a66b762b34cf741
Ariady Putra <ariady.putra@gmail.com>
2023-01-27 17:04:16 +0700

    Adjust pages order

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,10 +9,10 @@ const Navbar = () => {
             <div><img src='/logo1.jpg' alt='logo' /></div> 
             <div className='hidden md:flex gap-6'>
                 <Link href='/'><a className= 'hover:text-[#ff4D41]'>Home</a></Link>
-                <Link href='/aboutUs'><a className= 'hover:text-[#ff4D41]'>About us</a></Link>
                 <Link href='/openDCA'><a className= 'hover:text-[#ff4D41]'>Open DCA</a></Link>
-                <Link href='/swapDCA'><a className= 'hover:text-[#ff4D41]'>Swap DCA</a></Link>
                 <Link href='/portfolio'><a className= 'hover:text-[#ff4D41]'>Portfolio</a></Link>
+                <Link href='/swapDCA'><a className= 'hover:text-[#ff4D41]'>Swap DCA</a></Link>
+                <Link href='/aboutUs'><a className= 'hover:text-[#ff4D41]'>About us</a></Link>
             </div>
             <a className='hidden md:flex border border-[#ff4D41] px-2 py-2 bg-[#ffffff] text-[#ff4D41] rounded-[5px] items-center gap-2 font-bold hover:bg-[#ff4D41] hover:text-white transition duration-200'>
                 <WalletConnect />
